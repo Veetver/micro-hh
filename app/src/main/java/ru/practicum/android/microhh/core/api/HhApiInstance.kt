1f0b2421222147ada3c6b7f38738e01e61ac131c
Anastasiya <personalnastya8@gmail.com>
2025-05-24 20:35:19 +0300

    micro-hh 7 Retrofit base code

--- app/src/main/java/ru/practicum/android/microhh/core/api/HhApiInstance.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/api/HhApiInstance.kt
@@ -1,15 +1,21 @@
 package ru.practicum.android.microhh.core.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object HhApiInstance {
     private const val baserUrl = "https://api.hh.ru"
 
+    private val gson: Gson = GsonBuilder()
+        .serializeNulls()
+        .create()
+
     val HHService: HhApi by lazy {
         Retrofit.Builder()
             .baseUrl(baserUrl)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(HhApi::class.java)
     }
--- app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
@@ -1,9 +1,16 @@
 package ru.practicum.android.microhh.core.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import ru.practicum.android.microhh.BuildConfig
 import ru.practicum.android.microhh.R
+import ru.practicum.android.microhh.core.api.HhApiInstance
+import ru.practicum.android.microhh.core.models.VacancyResponse
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +22,29 @@ class RootActivity : AppCompatActivity() {
     }
 
     private fun networkRequestExample(accessToken: String) {
-        // ...
+        HhApiInstance.HHService.vacancies("", "Bearer $accessToken").enqueue(object : Callback<VacancyResponse> {
+            override fun onResponse(call: Call<VacancyResponse>,
+                                    response: Response<VacancyResponse>
+            ) {
+                Log.d("API_RESPONSE", "Response id success ? - " + response.isSuccessful)
+                if (response.isSuccessful) {
+                    val vacancyResponse = response.body()
+                    Log.d("API_RESPONSE", "Total Found: ${vacancyResponse?.found}")
+                    Log.d("API_RESPONSE", "Current Page: ${vacancyResponse?.page}")
+                    Log.d("API_RESPONSE", "Items Per Page: ${vacancyResponse?.per_page}")
+
+                    vacancyResponse?.items?.forEach { vacancy ->
+                        Log.d("API_RESPONSE", "Vacancy Name: ${vacancy.name}")
+                        Log.d("API_RESPONSE", "Employer: ${vacancy.employer?.name}")
+                    }
+                } else {
+                    Log.e("API_ERROR", "Error: ${response.code()}")
+                }
+            }
+            override fun onFailure(call: Call<VacancyResponse>, t: Throwable) {
+                Log.e("API_FAILURE", "Request failed: ${t.message}")
+            }
+        })
     }
 
 }
