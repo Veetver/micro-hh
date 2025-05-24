9fd83c5677c03cf1000109694e17da03ff5185cc
Xaxaido <xaxaido@gmail.com>
2025-05-24 20:20:33 +0300

    10 - Настройка навигации

--- app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
@@ -10,16 +10,18 @@ import ru.practicum.android.microhh.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
-    private lateinit var navController: NavController
-    private lateinit var navHostFragment: NavHostFragment
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = requireNotNull(_binding) {
+        "ViewBinding cannot be null"
+    }
+    private var navController: NavController? = null
+    private var navHostFragment: NavHostFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityRootBinding.inflate(layoutInflater)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
         //setupUI()
@@ -32,10 +34,10 @@ class RootActivity : AppCompatActivity() {
 
     private fun setupUI() {
         //navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        navController = navHostFragment?.navController
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val currentFragment = navHostFragment.childFragmentManager.fragments.lastOrNull()
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            val currentFragment = navHostFragment?.childFragmentManager?.fragments?.lastOrNull()
 
             when (destination.id) {
                 else -> {}
@@ -44,7 +46,12 @@ class RootActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController?.navigateUp() ?: false || super.onSupportNavigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
