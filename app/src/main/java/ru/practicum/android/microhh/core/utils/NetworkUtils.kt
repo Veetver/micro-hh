d1972f613b4b90f3923f6fc407622db8b146d388
Anastasiya <personalnastya8@gmail.com>
2025-05-25 15:15:28 +0300

    micro-hh 6 utils

--- app/src/main/java/ru/practicum/android/microhh/core/utils/NetworkUtils.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/utils/NetworkUtils.kt
@@ -11,8 +11,8 @@ object NetworkUtils {
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         val network = connectivityManager.activeNetwork ?: return false
-        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
 
-        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+        return capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
     }
 }
