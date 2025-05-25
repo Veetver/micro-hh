c24ee0aa020ce35c32302d84eb2d7a1f084a750d
Anastasiya <personalnastya8@gmail.com>
2025-05-25 14:53:15 +0300

    micro-hh 6 utils

--- app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/presentation/ui/RootActivity.kt
@@ -1,15 +1,19 @@
 package ru.practicum.android.microhh.core.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.microhh.BuildConfig
 import ru.practicum.android.microhh.R
+import ru.practicum.android.microhh.core.utils.NetworkUtils
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
-
+        if (NetworkUtils.isNetworkAvailable(this)) {
+            Log.d("NETWORK", "Network is available")
+        }
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
     }
