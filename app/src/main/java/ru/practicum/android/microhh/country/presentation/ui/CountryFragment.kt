7010c394769bce5270d9055c7b3d27db1d49f993
Xaxaido <xaxaido@gmail.com>
2025-05-26 11:02:04 +0300

    Правки по detekt

--- app/src/main/java/ru/practicum/android/microhh/country/presentation/ui/CountryFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/country/presentation/ui/CountryFragment.kt
@@ -1,6 +1,18 @@
-package ru.practicum.android.microhh.search.presentation.ui
+package ru.practicum.android.microhh.country.presentation.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class CountryFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        // TODO
+    }
 }
--- app/src/main/java/ru/practicum/android/microhh/workplace/presentation/ui/WorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/workplace/presentation/ui/WorkplaceFragment.kt
@@ -1,6 +1,18 @@
-package ru.practicum.android.microhh.search.presentation.ui
+package ru.practicum.android.microhh.workplace.presentation.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class WorkplaceFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        // TODO
+    }
 }
