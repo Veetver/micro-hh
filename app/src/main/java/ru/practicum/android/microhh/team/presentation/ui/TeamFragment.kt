5d08c3ea84ccf7a7e481bca51c21c25ad1da9c22
Peter <klintsov.p@mail.ru>
2025-05-26 22:56:35 +0300

    доделан фрагмент

--- app/src/main/java/ru/practicum/android/microhh/team/presentation/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/team/presentation/ui/TeamFragment.kt
@@ -1,18 +1,28 @@
 package ru.practicum.android.microhh.team.presentation.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.practicum.android.microhh.databinding.FragmentTeamBinding
 
 class TeamFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private var _binding: FragmentTeamBinding? = null
+    private val binding get() = _binding!!
 
-        setListeners()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTeamBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    private fun setListeners() {
-        // TODO
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
