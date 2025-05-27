0d557718419e6de1c981bc6eb74c62c9affd6eca
Peter <klintsov.p@mail.ru>
2025-05-27 20:27:46 +0300

    унаследован от BaseFragment

--- app/src/main/java/ru/practicum/android/microhh/team/presentation/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/team/presentation/ui/TeamFragment.kt
@@ -1,28 +1,7 @@
 package ru.practicum.android.microhh.team.presentation.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import ru.practicum.android.microhh.core.presentation.ui.fragment.BaseFragment
 import ru.practicum.android.microhh.databinding.FragmentTeamBinding
 
-class TeamFragment : Fragment() {
+class TeamFragment : BaseFragment<FragmentTeamBinding>(FragmentTeamBinding::inflate)
 
-    private var _binding: FragmentTeamBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentTeamBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
