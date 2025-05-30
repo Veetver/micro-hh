46dcf8a687e4f13fa37a57e08092d9ba15e7729c
Anastasiya <personalnastya8@gmail.com>
2025-05-30 22:31:49 +0300

    micro-hh 47 import

--- app/src/main/java/ru/practicum/android/microhh/di/NetworkModule.kt
+++ app/src/main/java/ru/practicum/android/microhh/di/NetworkModule.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.microhh.di
 
-import org.koin.dsl.module
-import com.google.gson.GsonBuilder
 import com.google.gson.Gson
-import okhttp3.OkHttpClient
+import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.microhh.BuildConfig
