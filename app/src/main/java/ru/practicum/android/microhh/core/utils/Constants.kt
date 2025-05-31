be7af1ed7ebd8d31c262ddb97f4971cde20802f5
Xaxaido <xaxaido@gmail.com>
2025-05-31 17:35:24 +0300

    40 - Выполнение поискового запроса с полной проработкой слоёв Data, Domain, Presentation

--- app/src/main/java/ru/practicum/android/microhh/core/utils/Constants.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/utils/Constants.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.microhh.core.utils
 
 object Constants {
-
     const val HTTP_OK = 200
     const val INTERNAL_SERVER_ERROR = 500
     const val USER_INPUT_DELAY = 2000L
