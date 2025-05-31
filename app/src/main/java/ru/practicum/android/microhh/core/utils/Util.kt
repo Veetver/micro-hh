839580049f69fd65b20c70a366c97bd7cdfd8d40
Xaxaido <xaxaido@gmail.com>
2025-05-31 19:42:14 +0300

    Правки по detekt

--- app/src/main/java/ru/practicum/android/microhh/core/utils/Util.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/utils/Util.kt
@@ -5,9 +5,11 @@ object Util {
     private const val FORM_SINGLE = 0
     private const val FORM_FEW = 1
     private const val FORM_MANY = 2
+    private const val TRAILING_1 = 1
+    private val TRAILING_FEW = setOf(2, 3, 4)
     private const val TENS = 10
     private const val HUNDREDS = 100
-    
+
     fun formatValue(value: Int, valueName: String): String {
         val valueNameToFormat = valueName.split(",")
         val preLastNumber = value % HUNDREDS / TENS
@@ -18,8 +20,8 @@ object Util {
         }
 
         return when (lastNumber) {
-            1 -> "$value ${valueNameToFormat[FORM_SINGLE]}"
-            2, 3, 4 -> "$value ${valueNameToFormat[FORM_FEW]}"
+            TRAILING_1 -> "$value ${valueNameToFormat[FORM_SINGLE]}"
+            in TRAILING_FEW -> "$value ${valueNameToFormat[FORM_FEW]}"
             else -> "$value ${valueNameToFormat[FORM_MANY]}"
         }
     }
