fcd142b1ad8b5d2bb269fee0142737d1bc3038e6
Anastasiya <personalnastya8@gmail.com>
2025-05-31 21:06:46 +0300

    micro-hh 46

--- app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
@@ -5,9 +5,8 @@ import android.view.View
 import androidx.core.view.isVisible
 import ru.practicum.android.microhh.core.presentation.ui.fragment.BaseFragment
 import ru.practicum.android.microhh.databinding.FragmentFavoritesBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.practicum.android.microhh.core.domain.models.JobInfo
 import ru.practicum.android.microhh.favorites.presentation.ui.interfaces.FavoriteJobScreenState
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavoritesBinding::inflate) {
 
@@ -19,15 +18,9 @@ class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavorit
             if (screenState is FavoriteJobScreenState.FavoriteContent) {
                 if (screenState.jobs.isNullOrEmpty()) {
                     showSearchNotFoundView(true)
-                } else {
-                    handleJobData(screenState.jobs)
                 }
             }
         }
-    }
-
-    private fun handleJobData(resultList: List<JobInfo>?) {
-
     }
 
     private fun showSearchNotFoundView(isVisible: Boolean) {
