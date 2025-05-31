3e8a48a41a8313d4c5f99012891499a055773729
Anastasiya <personalnastya8@gmail.com>
2025-05-31 21:35:02 +0300

    micro-hh 46

--- app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
@@ -3,10 +3,10 @@ package ru.practicum.android.microhh.favorites.presentation.ui
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.microhh.core.presentation.ui.fragment.BaseFragment
 import ru.practicum.android.microhh.databinding.FragmentFavoritesBinding
 import ru.practicum.android.microhh.favorites.presentation.ui.interfaces.FavoriteJobScreenState
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavoritesBinding::inflate) {
 
