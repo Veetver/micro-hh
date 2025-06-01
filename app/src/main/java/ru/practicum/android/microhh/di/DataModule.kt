ebe087255d340979165758fd1fbb741a7c847b08
Anastasiya <personalnastya8@gmail.com>
2025-06-01 14:30:20 +0300

    micro-hh 46 cr updates

--- app/src/main/java/ru/practicum/android/microhh/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/microhh/di/DataModule.kt
@@ -3,9 +3,15 @@ package ru.practicum.android.microhh.di
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.microhh.core.data.network.RetrofitNetworkClient
 import ru.practicum.android.microhh.core.db.AppDataBase
+import ru.practicum.android.microhh.search.data.impl.RetrofitNetworkClientImpl
 
 val dataModule = module {
+
+    single<RetrofitNetworkClient> {
+        RetrofitNetworkClientImpl(get())
+    }
 
     single {
         Room.databaseBuilder(
@@ -16,10 +22,6 @@ val dataModule = module {
     }
 
     /*
-    single<RetrofitNetworkClient> {
-        RetrofitNetworkClientImpl(get())
-    }
-
     single {
         androidContext().getSharedPreferences(androidContext()
             .getString(R.string.prefs_file_name), Context.MODE_PRIVATE)
