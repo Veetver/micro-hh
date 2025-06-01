d24d920533e6e83d9283c4749da9f157b1b5ab4f
Anastasiya <personalnastya8@gmail.com>
2025-06-01 17:53:43 +0300

    micro-hh 46

--- app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesViewModel.kt
+++ app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesViewModel.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
