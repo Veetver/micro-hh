793e9df4d60fa3f76f99e94b95d700e6576fd0a3
Xaxaido <xaxaido@gmail.com>
2025-06-05 17:48:01 +0300

    Добавлен обработчик кликов на экране избранного, правки в navGraph

--- app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/microhh/favorites/presentation/ui/FavoritesFragment.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.microhh.favorites.presentation.ui
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.microhh.core.presentation.ui.component.recycler.ItemAnimator
@@ -14,14 +15,18 @@ import ru.practicum.android.microhh.core.resources.VisibilityState.NoData
 import ru.practicum.android.microhh.core.resources.VisibilityState.Results
 import ru.practicum.android.microhh.core.resources.VisibilityState.ViewsList
 import ru.practicum.android.microhh.core.resources.VisibilityState.VisibilityItem
+import ru.practicum.android.microhh.core.utils.Constants
+import ru.practicum.android.microhh.core.utils.Debounce
 import ru.practicum.android.microhh.databinding.FragmentFavoritesBinding
 import ru.practicum.android.microhh.favorites.presentation.FavoritesViewModel
+import ru.practicum.android.microhh.search.presentation.ui.SearchFragmentDirections
 
 class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavoritesBinding::inflate) {
 
     private val viewModel: FavoritesViewModel by viewModel()
     private var vacancyAdapter: VacancyAdapter? = null
     private var visibility: ViewsList? = null
+    private var isClickEnabled = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -52,7 +57,15 @@ class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavorit
             )
         )
 
-        vacancyAdapter = VacancyAdapter()
+        vacancyAdapter = VacancyAdapter { vacancy ->
+            if (isClickEnabled) {
+                isClickEnabled = false
+                Debounce<Any>(Constants.BUTTON_ENABLED_DELAY, lifecycleScope) { isClickEnabled = true }.start()
+            }
+            findNavController().navigate(
+                SearchFragmentDirections.openVacancyDetails(vacancy.id)
+            )
+        }
         binding.jobList.adapter = vacancyAdapter
         binding.jobList.itemAnimator = ItemAnimator()
     }
