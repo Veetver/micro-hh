c1f0eb794d470343f6b9add198d63cd8e791c0da
Veetver <khabiev2012@yandex.ru>
2025-06-05 22:34:46 +0300

    Добавлены запросы для регионов и стран

--- app/src/main/java/ru/practicum/android/microhh/core/data/network/HhService.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/data/network/HhService.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.microhh.core.data.network
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.practicum.android.microhh.core.domain.models.Area
 import ru.practicum.android.microhh.search.data.dto.VacancyResponse
 import ru.practicum.android.microhh.vacancy.data.dto.VacancyDetailsResponse
 
@@ -18,4 +19,15 @@ interface HhService {
     suspend fun getVacancyDetails(
         @Path("vacancy_id") id: String
     ): VacancyDetailsResponse
+
+    @GET("/areas/countries")
+    suspend fun getCountries(): List<Area>
+
+    @GET("/areas")
+    suspend fun getAreas(): List<Area>
+
+    @GET("/areas/{area_id}")
+    suspend fun getAreaById(
+        @Path("area_id") id: String
+    ): List<Area>
 }
--- app/src/main/java/ru/practicum/android/microhh/core/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/microhh/core/data/network/RetrofitNetworkClient.kt
@@ -1,10 +1,14 @@
 package ru.practicum.android.microhh.core.data.network
 
 import ru.practicum.android.microhh.core.data.dto.Response
+import ru.practicum.android.microhh.country.data.dto.AreaRequest
 import ru.practicum.android.microhh.search.data.dto.RetrofitSearchRequest
 import ru.practicum.android.microhh.vacancy.data.dto.RetrofitVacancyDetailsRequest
 
 interface RetrofitNetworkClient {
     suspend fun doRequest(dto: RetrofitSearchRequest): Response
     suspend fun getVacancy(dto: RetrofitVacancyDetailsRequest): Response
+    suspend fun getCountries(): Response
+    suspend fun getAreas(): Response
+    suspend fun getAreaById(dto: AreaRequest): Response
 }
