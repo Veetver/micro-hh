a235b31ab633fe9b013d745061cb1948d5a9396f
Veetver <khabiev2012@yandex.ru>
2025-06-08 22:23:07 +0300

    Удалены лишние модели данных, исправлено использование.

--- app/src/main/java/ru/practicum/android/microhh/core/domain/models/Country.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.microhh.core.domain.models
-
-data class Country(
-    val id: String,
-    val name: String
-)
--- app/src/main/java/ru/practicum/android/microhh/country/domain/api/CountrySearchRepository.kt
+++
@@ -1,8 +0,0 @@
-package ru.practicum.android.microhh.country.domain.api
-
-import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.microhh.core.resources.CountrySearchState
-
-interface CountrySearchRepository {
-    fun getCountries(): Flow<CountrySearchState>
-}
